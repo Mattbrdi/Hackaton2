94628da9d44f483aa0467501b82ec726c119ceed
mattis brdi <matborderies@mail.com>
2024-02-02 13:05:28 +0100

    affichage progressif de la carte

--- map.py
+++ map.py
@@ -1,5 +1,9 @@
 import pygame
+import main
+import perso 
 import sys
+
+carre = 32
 class Map:
     def __init__(self, filename):
         self.map = []
@@ -8,32 +12,68 @@ class Map:
                 self.map.append(list(line)[:-1])
         
         self.map_decouverte = [[' ' for _ in range(len(row))] for row in self.map]
-        print(self.map_decouverte)
 
     def draw_map(self, screen):
         for i in range(len(self.map_decouverte)):
             for j in range(len(self.map_decouverte[i])):
                 if self.map_decouverte[i][j] == ' ':
-                    pygame.draw.rect(screen, (0, 0, 0), (j*32, i*32, 32, 32))
+                    pygame.draw.rect(screen, (0, 0, 0), (j*carre, i*carre, carre, carre))
                 elif self.map_decouverte[i][j] == '.':
-                    pygame.draw.rect(screen, (255,255,255), (j*32, i*32, 32, 32))
+                    pygame.draw.rect(screen, (255,255,255), (j*carre, i*carre, carre, carre))
                 elif self.map_decouverte[i][j] == '#':
-                    pygame.draw.rect(screen, (100, 100, 100), (j*32, i*32, 32, 32))
+                    pygame.draw.rect(screen, (100, 100, 100), (j*carre, i*carre, carre, carre))
                 elif self.map_decouverte[i][j] == '+':
-                    pygame.draw.rect(screen, (255, 255, 0), (j*32, i*32, 32, 32))
+                    pygame.draw.rect(screen, (255, 255, 0), (j*carre, i*carre, carre, carre))
                 elif self.map_decouverte[i][j] == '-' or self.map_decouverte[i][j] == '|': 
-                    pygame.draw.rect(screen, (0, 0, 255), (j*32, i*32, 32, 32)) 
-    def update_map(self, x, y):
+                    pygame.draw.rect(screen, (0, 0, 255), (j*carre, i*carre, carre, carre)) 
+    def decouvre(self, x, y):
         self.map_decouverte = self.map   
-
+    def cache(self, x, y):
+        self.map_decouverte[x][y] = ' '
 carte = Map('map.txt')
-def draw(screen):     
+
+def draw(screen, carte):  
+    carte.update_map(0,0)  
     carte.draw_map(screen)
     
-"""
+def point_with_caractere_next_to(carte, position, caractere):
+    points = [] 
+    if carte.map[position[0]-1][position[1]] == caractere:
+        points.append ((position[0]-1, position[1]))
+    if carte.map[position[0]+1][position[1]] == caractere :
+        points.append ((position[0]+1, position[1]))
+    if carte.map[position[0]][position[1]-1] == caractere :
+        points.append ((position[0], position[1]-1))
+    if carte.map[position[0]][position[1]+1] == caractere:
+        return points.append((position[0], position[1]+1))
+    return points
+
+def point_a_devoiler(carte, position): #position en (ligne, colonne)
+    point_a_devoiler = []
+    if carte.map[position[0]][position[1]] == '+':
+        file = [(position[0], position[1])]
+        while len(file)>0:
+            for point in point_with_caractere_next_to(carte, file[0], '.'):
+                if point not in point_a_devoiler and point not in file:
+                    file.append(point)
+            a= file.pop(0)
+            point_a_devoiler.append(a)
+                  
+    if carte.map[position[0]][position[1]] == '#':
+        point_a_devoiler.append((position[0]+1, position[1]))
+        point_a_devoiler.append((position[0]-1, position[1]))
+        point_a_devoiler.append((position[0], position[1]+1))
+        point_a_devoiler.append((position[0], position[1]-1))
+    return point_a_devoiler  
+def update_map(carte, perso):
+    position = perso.get_position()
+    for point in point_a_devoiler(carte, position):
+        carte.devoile(point[0], point[1])
+
+
 #-----------
 pygame.init()
-largeur, hauteur = 32*len(carte.map[0]), 32*len(carte.map)
+largeur, hauteur = carre*len(carte.map[0]), carre*len(carte.map)
 taille_fenetre = (largeur, hauteur)
 screen = pygame.display.set_mode(taille_fenetre)
 pygame.display.set_caption("screen")
@@ -46,7 +86,8 @@ while True:
             sys.exit()
             
     screen.fill(couleur_fond)
-    carte.draw_map(screen)
+    carte.draw_map(screen, carte)
     pygame.display.flip()
 
-    pygame.time.Clock().tick(60)"""
+    pygame.time.Clock().tick(60)
+    
